a6b5cc27ff0ab314449ea5b47e7e5c641a4c0300
Alejo9806 <mello0616@gmail.com>
2020-10-10 11:27:15 -0500

    get name parameter

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
         <title>PHP Test</title>
     </head>
     <body>
-        <?php echo '<p>Hello World</p>'; ?> 
+        <?php echo '<h1>Hello '.htmlspecialchars($_GET["name"]) .'</h1>'; ?>
     </body>
 </html>
